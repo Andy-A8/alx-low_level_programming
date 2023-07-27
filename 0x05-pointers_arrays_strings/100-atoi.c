fdd7fabb611fdc31ce8f21fafbfcfc319d4da4bc
Andy-A8 <aaallotey006@st.ug.edu.gh>
2023-07-27 07:59:12 -0700

    A function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,13 +15,19 @@ int _atoi(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
+		{
 			sign = -1;
+		}
 
-		else if (s[i] >= '0' && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
+		{
 			num = (num * 10) + (s[i] - '0');
 
-		else if (s[i + 1] < '0' || s[i + 1] > '9')
+		if (s[i + 1] < '0' || s[i + 1] > '9')
+		{
 			break;
+		}
+		}
 	}
 	return (sign * num);
 }
