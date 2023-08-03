2c4fc9a111c7b11462436ac406363ad274966346
Andy-A8 <aaallotey006@st.ug.edu.gh>
2023-08-03 11:25:19 -0700

    Updated main.h file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s - The string to be capitalized
+ *
+ * Return: A pointer to capitalized string.
+ */
+char *cap_string(char *s)
+{
+
