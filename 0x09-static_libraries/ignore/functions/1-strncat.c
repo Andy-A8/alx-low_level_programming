bd6b97af59bb4e8fe74a00f289d569942dfb0dd7
Andy-A8 <aaallotey006@st.ug.edu.gh>
2023-08-03 17:23:11 -0700

    Creating the static library libmy.a

---
+++ 0x09-static_libraries/ignore/functions/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: A pointer to the string to be concatenated on
+ * @src: The source string to be appendded to @dest
+ * @n: The number of bytes from src
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+
+	return (dest);
+}
