027f30fd28da886583532db11f4009f2a30f7c56
Andy-A8 <aaallotey006@st.ug.edu.gh>
2023-08-15 20:05:51 -0700

    Defined a new type with elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog data.
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: A new type describing a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
