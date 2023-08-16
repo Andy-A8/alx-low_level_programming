eb152d6816792905b2b41e0d2392482034017961
Andy-A8 <aaallotey006@st.ug.edu.gh>
2023-08-15 21:21:39 -0700

    A new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,17 @@
  *
  * Description: A new type describing a dog.
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
