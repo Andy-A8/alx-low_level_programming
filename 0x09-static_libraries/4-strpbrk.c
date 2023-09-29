ea6285b4f99d57e465b09461ed9d7bb041511164
Andy-A8 <aaallotey006@st.ug.edu.gh>
2023-09-29 11:46:51 -0700

    Create the static library containing all functions

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ * @s: The string to be searched
+ * @accept: The set of bytes to be searched for
+ *
+ * Return: Nothing.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: The string to be searched
+ * @needle: The substring to be located
+ *
+ * Return: Nothing
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
