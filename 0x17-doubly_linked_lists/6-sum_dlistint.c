8a566b3b858e66b4a529747f004f7876696ea739
Andy-A8 <aaallotey006@st.ug.edu.gh>
2023-12-09 04:50:10 -0800

    Addressed warning description message

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,7 +3,7 @@
 
 /**
  * sum_dlistint - Sums of all the data of a dlistint_t linked list.
- * *head: The head of the list.
+ * @head: The head of the list.
  *
  * Return: The sum of all the data - if function succeeds,
  *	Or 0 - If the list is empty.
