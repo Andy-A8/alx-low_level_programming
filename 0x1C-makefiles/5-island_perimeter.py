9119e0942f774c6b7d1d305a513860efa243e247
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-20 21:08:41 -0700

    Updated island perimeter function

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,13 +19,14 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(height):
         for j in range(width):
-            if j == 0 or grid[1][j - 1] == 0:
-                perimeter += 1
-            if j == width - 1 or grid[i][j + 1] == 0:
-                perimeter += 1
-            if i == 0 or grid[i - 1][j] == 0:
-                perimeter += 1
-            if i == height - 1 or grid[i + 1][j] == 0:
-                perimeter += 1
+            if grid[i][j] == 1:
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == width - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == height - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
 
     return perimeter
