1f2341c1ac826a3b090fb6b8f6169a1e1d22de72
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-05-18 10:33:17 -0700

    Updated header file with data structures

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,13 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /**
- * struct listint_s - Singly linked list
+ * struct listint_s - singly linked list
  *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct listint_s
 {
@@ -19,12 +22,14 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct skiplist_s - Singly linked list with express lane
+ * struct skiplist_s - Singly linked list with an express lane
  *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in express lane
+ *
+ * Description : singly linked list node structure with an express lane
  */
 typedef struct skiplist_s
 {
